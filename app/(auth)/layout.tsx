cb440a8ec2feb1aac21f6f39ee70a87d0ada4fdc
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-07-15 15:53:01 +0100

    Fix login register layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function AutLayout({ children }: { children: React.ReactNode }) {
-  return <div className="bg-red-500 h-full">{children}</div>;
+  return <div className="h-full flex items-center justify-center">{children}</div>;
 }
