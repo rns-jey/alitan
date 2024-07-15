14c2553e266df77b82695e1b7bfeaf2f95372907
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-07-15 17:56:05 +0100

    Add auto redirect to auth

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-export default clerkMiddleware();
+import { auth, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { request } from "http";
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
+
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
