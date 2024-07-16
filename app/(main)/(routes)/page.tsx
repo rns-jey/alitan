178bd7fcf3615f2e3a28dce319d7a9c71a1f33d0
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-07-16 11:24:50 +0100

    Add light and dark mode themes

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/atoms/button";
+import { ModeToggle } from "@/components/molecules/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div>This is a protected route.</div>;
+  return (
+    <div>
+      <UserButton />
+      <ModeToggle />
+    </div>
+  );
 }
