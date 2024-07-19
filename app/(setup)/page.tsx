de5190049caee168bd1929cbd417e87a764fa71b
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-07-19 09:47:28 +0100

    Setup prisma and supabase

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,25 @@
+import { db } from "@/lib/db";
+import initialProfile from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+export default async function SetupPage() {
+  const profile = await initialProfile();
+
+  if (!profile) return;
+
+  const server = await db.servers.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <div>Create a server</div>;
+}
