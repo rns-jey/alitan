0d71daadbcca81c997e0381f8bb767c810565519
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-07-19 11:29:27 +0100

    Fix dialog header

--- components/molecules/initial-modal.tsx
+++ components/molecules/initial-modal.tsx
@@ -36,10 +36,10 @@ export default function InitialModal() {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">Customize your server</DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can always change it later.
+          </DialogDescription>
         </DialogHeader>
-        <DialogDescription className="text-center text-zinc-500">
-          Give your server a personality with a name and an image. You can always change it later.
-        </DialogDescription>
       </DialogContent>
     </Dialog>
   );
