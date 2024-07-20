07d6568c3646188925797b52616a76a2e903859f
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-07-20 16:52:05 +0100

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,9 @@
-import { auth, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { request } from "http";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
+  if (!isPublicRoute(request)) auth().protect();
 });
 
 export const config = {
