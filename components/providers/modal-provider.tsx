e98a15e130c430c4d97913f78be3fa7eda9c421c
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-07-22 17:58:18 +0100

    Build modal probider

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,10 @@
+"use client";
+import CreateServerModal from "../molecules/create-server-modal";
+
+export default function modalProvider() {
+  return (
+    <>
+      <CreateServerModal />
+    </>
+  );
+}
