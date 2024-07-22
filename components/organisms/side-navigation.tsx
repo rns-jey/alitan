8cd940d475786cde6016f748fea5504d0f439de0
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-07-22 07:55:28 +0100

    Build server button

--- components/organisms/side-navigation.tsx
+++ components/organisms/side-navigation.tsx
@@ -4,6 +4,8 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import NavigationAction from "./navigation-action";
 import { Separator } from "@/components/atoms/separator";
+import { ScrollArea } from "@/components/molecules/scroll-area";
+import NavigationItem from "@/components/molecules/navigation-item";
 
 export default async function SideNavigation() {
   const profile = await currentProfile();
@@ -16,6 +18,13 @@ export default async function SideNavigation() {
     <div className="space-y-4 flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
       <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem id={server.id} name={server.name} imageUrl={server.imageUrl} />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 }
