0296a2602e3612b633108d99379403b6d124313e
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-07-25 08:00:28 +0100

    Add invite modal and functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,10 +1,12 @@
 "use client";
 import CreateServerModal from "../molecules/create-server-modal";
+import InviteModal from "../molecules/invite-modal";
 
 export default function ModalProvider() {
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
     </>
   );
 }
