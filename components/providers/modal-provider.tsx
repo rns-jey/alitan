db545eadfa014a69e160044c83c168028ff22191
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-07-29 10:32:04 +0100

    Init build members modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 import CreateServerModal from "../molecules/create-server-modal";
 import EditServerModal from "../molecules/edit-server";
 import InviteModal from "../molecules/invite-modal";
+import MembersModal from "../molecules/members-modal";
 
 export default function ModalProvider() {
   return (
@@ -9,6 +10,7 @@ export default function ModalProvider() {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
     </>
   );
 }
