20fa4a2412f7aff81d354bfa065cbbb366324665
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-07-29 10:27:31 +0100

    Add server update functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import CreateServerModal from "../molecules/create-server-modal";
+import EditServerModal from "../molecules/edit-server";
 import InviteModal from "../molecules/invite-modal";
 
 export default function ModalProvider() {
@@ -7,6 +8,7 @@ export default function ModalProvider() {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
     </>
   );
 }
