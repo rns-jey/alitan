e077e4611dc4b3addc5c19e8200b10f328ee0249
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-08-16 21:10:36 +0100

    Build channel header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,3 +1,39 @@
-export default function ChannelIdPage() {
-  return <div>ChannelIdPage</div>;
+import ChatHeader from "@/components/molecules/chat-header";
+import currentProfile from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
+  const profile = await currentProfile();
+
+  if (!profile) return auth().redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      profileId: profile.id,
+      serverId: params.serverId,
+    },
+  });
+
+  if (!channel || !member) return redirect("/dashboard");
+
+  return (
+    <div className="bg-white dark:bg-[#313338]">
+      <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+    </div>
+  );
 }
