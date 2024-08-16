9dbcd0a138e08c57f57fcb6dde732fff6b83630f
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-08-16 21:28:22 +0100

    Build mobile navigation menu

--- components/molecules/chat-header.tsx
+++ components/molecules/chat-header.tsx
@@ -1,4 +1,5 @@
-import { Hash, Menu } from "lucide-react";
+import { Hash } from "lucide-react";
+import MobileToggle from "../atoms/mobile-toggle";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -10,7 +11,7 @@ interface ChatHeaderProps {
 export default function ChatHeader({ serverId, name, type, imageUrl }: ChatHeaderProps) {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <Menu />
+      <MobileToggle serverId={serverId} />
       {type === "channel" && <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
     </div>
