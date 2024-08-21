d42f852cb4d623c7146c4735885bb67969cc874a
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-08-21 20:50:21 +0100

    Build direct message feature

--- components/molecules/chat/chat-messages.tsx
+++ components/molecules/chat/chat-messages.tsx
@@ -84,7 +84,7 @@ export default function ChatMessages({
   return (
     <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
       {!hasNextPage && <div className="flex-1" />}
-      {!hasNextPage && <ChatWelcome name={name} type="channel" />}
+      {!hasNextPage && <ChatWelcome name={name} type={type} />}
       {hasNextPage && (
         <div className="flex justify-center">
           {isFetchingNextPage ? (
